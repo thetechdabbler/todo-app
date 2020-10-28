064e9dfee0c008665b1c4eb953430c7a6dfe568f
Manish Choudhary <codespeech@pm.me>
2020-10-28 17:13:20 +0530

    Fixed task deletion issue

--- src/components/Tasklist.js
+++ src/components/Tasklist.js
@@ -1,19 +1,40 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Tasklist() {
-  const [tasks, settasks] = React.useState([]);
+  const [tasks, settasks] = React.useState(() => {
+    return JSON.parse(window.localStorage.getItem("tasks")) || [];
+  });
 
   const handleAddTask = function () {
     const newTasks = [...tasks];
-    newTasks.push({ title: "" });
+    newTasks.push({ title: "", id: uuidv4() });
     settasks(newTasks);
   };
+
+  const handleUpdateTask = function (e) {
+    const itemIndex = e.target.dataset.index;
+    const newTasks = [...tasks];
+    newTasks[itemIndex].title = e.target.value;
+    settasks(newTasks);
+  };
+
+  const handleDelete = function (e) {
+    const itemIndex = e.target.dataset.index;
+    const newTasks = [...tasks];
+    newTasks.splice(itemIndex, 1);
+    settasks([...newTasks]);
+  };
+
+  React.useEffect(() => {
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
   return (
     <div>
       <div className="">
         <button
           onClick={handleAddTask}
-          className=" text-2xl  text-bold m-3 py-2 px-4 border-solid border-4 border-gray-600"
+          className="text-2xl  text-bold m-3 py-2 px-4 border-solid border-4 border-gray-600"
         >
           +
         </button>
@@ -23,18 +44,28 @@ export default function Tasklist() {
           {tasks.map((task, i) => {
             return (
               <li
-                className="group bg-white hover:bg-teal-500 border-solid border-t border-grey-600"
-                key={i}
+                className="group bg-white hover:border hover:border-solid hover:border-t hover:border-grey-600 "
+                key={task.id}
               >
-                {i + 1}.{" "}
-                <input
-                  className="bg-transparent focus:text-white focus:text-bold p-4 bg-none outline-none min-w-full"
-                  defaultValue={task.title}
-                  placeholder="Enter task details"
-                  onChange={(e) => {
-                    task.title = e.target.value;
-                  }}
-                />
+                <div className="flex">
+                  <div className="flex-initial  p-4 w-4"> {i + 1}. </div>
+                  <div className="flex-auto">
+                    <input
+                      className="bg-transparent p-4 focus:text-bold bg-none outline-none min-w-full"
+                      defaultValue={task.title}
+                      placeholder="Enter task details"
+                      data-index={i}
+                      onChange={handleUpdateTask}
+                    />
+                  </div>
+                  <button
+                    className="flex-initial"
+                    data-index={i}
+                    onClick={handleDelete}
+                  >
+                    Delete
+                  </button>
+                </div>
               </li>
             );
           })}
